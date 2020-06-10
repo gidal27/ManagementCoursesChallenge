ae4a0ef7d10f6ba011dbd0c72e58e465f50eae48
giovanni <giovanniayayi.dalmeida@gmail.com>
2020-06-10 07:24:16 -0700

    Creation of the management courses application

---
+++ IMNAT.School.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IMNAT.School.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
